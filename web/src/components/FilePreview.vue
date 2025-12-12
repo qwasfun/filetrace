0e14559dc7ae45b008a04dc0d17866dd85d7c6b1
qwas <git@qwas.fun>
2025-12-13 01:56:54 +0800

    fix: 移除 iframe 中的内容

--- web/src/components/FilePreview.vue
+++ web/src/components/FilePreview.vue
@@ -99,31 +99,11 @@
 
         <!-- PDF预览 -->
         <div v-else-if="isPdf(file.mime_type)" class="h-full">
-            <iframe
+          <iframe
             :src="`${serverUrl}/${file.download_url}#view=FitH`"
             class="w-full h-full border-0"
             title="PDF预览"
-          >
-            <div class="flex flex-col items-center justify-center h-full p-8">
-              <svg class="w-16 h-16 text-gray-400 mb-4" fill="currentColor" viewBox="0 0 20 20">
-                <path
-                  fill-rule="evenodd"
-                  d="M4 4a2 2 0 012-2h4.586A2 2 0 0112 2.586L15.414 6A2 2 0 0116 7.414V16a2 2 0 01-2 2H6a2 2 0 01-2-2V4z"
-                  clip-rule="evenodd"
-                />
-              </svg>
-              <p class="text-gray-600 dark:text-gray-400 text-center">
-                无法预览PDF文件<br />
-                <a
-                  :href="`${serverUrl}/${file.download_url}`"
-                  target="_blank"
-                  class="text-blue-500 hover:underline"
-                >
-                  点击下载查看
-                </a>
-              </p>
-            </div>
-          </iframe>
+          ></iframe>
         </div>
 
         <!-- 音频预览 -->
